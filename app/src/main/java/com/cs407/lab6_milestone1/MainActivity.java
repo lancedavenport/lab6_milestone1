24af1fdf17c5b56abf99d64bef322ba01cd4396a
lancedavenport <lance.davenport@icloud.com>
2023-11-02 14:41:03 -0500

    Changes

--- app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cs407.lab6_milestone1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -8,6 +9,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -49,6 +51,7 @@ public class MainActivity extends FragmentActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
         }
         else{
             mFusedLocationProviderClient.getLastLocation()
@@ -58,6 +61,9 @@ public class MainActivity extends FragmentActivity {
                                 mMap.addPolyline(new PolylineOptions().add(
                                         new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()),
                                         mDestinationLatLng));
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()))
+                                        .title("Current Location"));
 
                             }
                     });
