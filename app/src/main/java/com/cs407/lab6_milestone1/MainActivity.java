b76d274fdc7966aca7cfc033523eba5a2f64cb4b
lancedavenport <lance.davenport@icloud.com>
2023-11-02 13:21:40 -0500

    Commit

--- app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final LatLng mDestinationLatLng = new LatLng(43.0752218, -89.4028076);
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
